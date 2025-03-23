b52837e6b74667ce982cb14bafe3a0f656f24e3f
Clément Josso <clement.josso@epfl.ch>
2025-03-23 07:45:01 +0100

    feat: 4 pots immutable in garden

--- Scripts/jar_slot.gd
+++ Scripts/jar_slot.gd
@@ -4,6 +4,12 @@ class_name JarSlot
 var jar: Pot
 var plant: Plant
 var harvestable: bool = false
+signal right_clicked
 
 func _ready() -> void:
 	custom_minimum_size.y = custom_minimum_size.x * 2
+	
+func _input(event):
+	if !GlobalScript.itemInHand && Input.is_action_pressed("Right_click"):
+		print("right click")
+		right_clicked.emit()
