b1f9c728aa39fe453082f21e01c0db9ddb4c4ead
Sidonie Bouthors <sidonie.bouthors@epfl.ch>
2025-03-23 08:43:30 +0100

    inventory fixes

--- Inventory/slot.gd
+++ Inventory/slot.gd
@@ -1,15 +1,21 @@
-extends TextureButton
+extends AspectRatioContainer
 class_name Slot
 
-@onready var backgroundSprite: Sprite2D = $Background
-@onready var container: CenterContainer = $CenterContainer
-
+@onready var button: TextureButton = $SlotButton
+@onready var margin: MarginContainer = $SlotButton/Margin
 @onready var inventory : Inventory = Global.get_node("Inventory")
 var itemStackGui: ItemStackGUI
 
+func _ready():
+	var margin_value = 20
+	margin.add_theme_constant_override("margin_top", margin_value)
+	margin.add_theme_constant_override("margin_left", margin_value)
+	margin.add_theme_constant_override("margin_bottom", margin_value)
+	margin.add_theme_constant_override("margin_right", margin_value)
+
 func insert(isg: ItemStackGUI):
 	itemStackGui = isg
-	container.add_child(itemStackGui)
+	margin.add_child(itemStackGui)
 
 func takeItem() -> ItemGUI:
 	if inventory.remove(itemStackGui.item):
