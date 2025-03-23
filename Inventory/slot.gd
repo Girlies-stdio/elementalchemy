c6fe6e2e8c644ad4883ba3733a7f70e73709fa08
Clément Josso <clement.josso@epfl.ch>
2025-03-23 12:18:44 +0100

    color of emptySlot

--- Inventory/slot.gd
+++ Inventory/slot.gd
@@ -5,6 +5,7 @@ class_name Slot
 @onready var margin: MarginContainer = $SlotButton/Margin
 @onready var inventory : Inventory = Global.get_node("Inventory")
 var itemStackGui: ItemStackGUI
+var baseTexture: CompressedTexture2D
 
 func _ready():
 	var margin_value = 20
@@ -12,6 +13,7 @@ func _ready():
 	margin.add_theme_constant_override("margin_left", margin_value)
 	margin.add_theme_constant_override("margin_bottom", margin_value)
 	margin.add_theme_constant_override("margin_right", margin_value)
+	baseTexture = $SlotButton.texture_normal
 
 func insert(isg: ItemStackGUI):
 	itemStackGui = isg
@@ -29,31 +31,6 @@ func takeItem() -> ItemGUI:
 func _process(delta):
 	if itemStackGui:
 		if itemStackGui.amount != 0:
-			shade(false)
+			$SlotButton.texture_normal = baseTexture
 		else:
-			shade(true)
-			
-func shade(dark: bool) -> void:
-	if !dark:
-		$SlotButton.material = null
-		return
-	var shader = Shader.new()
-	var shader_material = ShaderMaterial.new()
-	var shader_code = """
-	shader_type canvas_item;
-
-	uniform float darkness : hint_range(0.0, 1.0) = 0.5;
-
-	void fragment() {
-		vec4 c = texture(TEXTURE, UV);
-		// On réduit la composante RGB en la multipliant
-		// Exemple : "1.0 - darkness" => 0.5 => 50% plus sombre
-		c.rgb *= (1.0 - darkness);
-		COLOR = c;
-	}
-"""
-	var mat = ShaderMaterial.new()
-	mat.shader = shader
-	mat.set_shader_parameter("darkness", 1.0)
-
-	$SlotButton.material = mat
+			$SlotButton.texture_normal = $SlotButton.texture_disabled
