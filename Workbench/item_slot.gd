1ff59e1a0895fd52f05f2c9c6f1f1aa8386b44f1
Clément Josso <clement.josso@epfl.ch>
2025-03-23 08:12:26 +0100

    fix: right click bugs

--- Workbench/item_slot.gd
+++ Workbench/item_slot.gd
@@ -6,6 +6,6 @@ signal right_clicked
 # The item currently in this slot
 var item : Item
 
-func _input(event):
+func _gui_input(event):
 	if !GlobalScript.itemInHand && Input.is_action_pressed("Right_click"):
 		right_clicked.emit()
