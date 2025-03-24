8bdaff8d0773d2c66f63347c11e89061044ea2b2
Sidonie Bouthors <sidonie.bouthors@epfl.ch>
2025-03-24 13:47:07 +0100

    refactor: snake_case folders

---
+++ scripts/global_script.gd
@@ -0,0 +1,44 @@
+extends Node
+
+var ALL_ITEMS : Array[Item]
+var itemInHand: ItemGUI
+
+var mouseItem: MouseItem
+
+func _ready():
+	var tier1 = preload("res://inventory/items/pots/pot_tier1.tres")
+	var tier2 = preload("res://inventory/items/pots/pot_tier2.tres")
+	var tier3 = preload("res://inventory/items/pots/pot_tier3.tres")
+	var tier4 = preload("res://inventory/items/pots/pot_tier4.tres")
+	ALL_ITEMS.append(tier1)
+	ALL_ITEMS.append(tier2)
+	ALL_ITEMS.append(tier3)
+	ALL_ITEMS.append(tier4)
+	if !RecipesLoader.is_node_ready():
+		await RecipesLoader.ready
+	var recipes = RecipesLoader.recipes
+	for recipe in recipes.values():
+		var plant = load("res://inventory/items/plants/"+ recipe["name"]+".tres")
+		ALL_ITEMS.append(plant)
+		
+func insertInHand(item: Item) -> ItemGUI:
+	if itemInHand: return null
+	var new_itemGUI = preload("res://scenes/item_gui.tscn").instantiate()
+	mouseItem.add_child(new_itemGUI)
+	if !new_itemGUI.is_node_ready():
+		await new_itemGUI.ready
+	new_itemGUI.item = item
+	new_itemGUI.set_texture(item.texture)
+	
+	itemInHand = new_itemGUI
+	
+	return itemInHand
+	
+func findItem(label: String) -> Item:
+	var items = ALL_ITEMS.filter(func(item) : return item.name == label)
+	return items[0] if items else null
+	
+func createMouseItem():
+	mouseItem = preload("res://components/mouse_item/mouse_item.tscn").instantiate()
+	get_tree().current_scene.add_child(mouseItem)
+	
