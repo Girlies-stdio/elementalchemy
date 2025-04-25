a2470c4803bb755e9841ea6106a49abaab618da7
Clément Josso <clement.josso@epfl.ch>
2025-04-26 00:59:49 +0200

    jar refactor pt3

--- inventory/tool_tip.gd
+++ inventory/tool_tip.gd
@@ -29,7 +29,7 @@ func _make_custom_tooltip(for_text):
 		#Show name, picture of the ingredients & pot it grows in
 		tooltip.get_node("Panel/VBoxContainer/HBoxContainer2/name").text = recipe["name"]
 		tooltip.get_node("Panel/VBoxContainer/HBoxContainer2/name_text").texture = GlobalScript.findItem(recipe["name"]).texture
-		var res = load("res://inventory/items/pots/pot_tier"+type+".tres")
+		var res = load("res://inventory/items/pots/jar_tier"+type+".tres")
 		tooltip.get_node("Panel/VBoxContainer/HBoxContainer2/pot").texture = res.texture
 		tooltip.custom_minimum_size = Vector2(300.,25.)
 		
@@ -55,7 +55,7 @@ func set_isg(new_isg : ItemStackGUI):
 	if isg.item == null:
 		show_tooltip = false
 		return
-	if isg.item.name.begins_with("pot_tier"):
+	if isg.item.name.begins_with("jar_tier"):
 		show_tooltip = false
 		return
 
